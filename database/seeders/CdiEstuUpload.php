8b3badfde4ac3afdc0ac096e21973761d5be949b
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2024-11-29 17:12:14 -0500

    add: cdc and cdi database upload

---
+++ database/seeders/CdiEstuUpload.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Parroquia;
+use App\Models\Representante;
+use App\Models\Cdi;
+use App\Models\Estudiante;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use SplFileObject;
+
+class CdiEstuUpload extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = new SplFileObject(database_path('seeders/cdc_estudiantes_subir.csv'));
+        $data->setFlags(SplFileObject::READ_CSV);
+        $data->setCsvControl(',');
+        $it = new \LimitIterator($data, 1);
+        foreach ($it as $row) {
+            $rep_row = ($row[0] ?? '');
+            $representante = Representante::where('cedula', $rep_row)->first();
+            if (!$representante) {
+                $representante = Representante::create([
+                    'cedula' => $rep_row,
+                    'last_name' => ($row[1] ?? ''),
+                    'first_name' => ($row[2] ?? ''),
+                    'phone' => ($row[3] ?? ''),
+                    'email' => ($row[4] ?? '')
+                ]);
+            }
+
+            $parroquia_row = ($row[8] ?? '');
+            $parroquia = Parroquia::where('name', $parroquia_row)->first();
+
+            if (!$parroquia) {
+                $parroquia = Parroquia::create([
+                    'name' => $parroquia_row
+                ]);
+            }
+
+            $barrio_row = ($row[9] ?? '');
+            $barrio = $parroquia->barrios()->where('name', $barrio_row)->first();
+            if (!$barrio) {
+                $barrio = $parroquia->barrios()->create([
+                    'name' => $barrio_row
+                ]);
+            }
+
+            $cdc_row = ($row[10] ?? '');
+            $cdc = Cdi::where('name', $cdc_row)->first();
+            if (!$cdc) {
+                $cdc = Cdi::create([
+                    'name' => $cdc_row
+                ]);
+            }
+
+            Estudiante::create([
+                'representante_id' => $representante->id,
+                'cedula' => ($row[5] ?? ''),
+                'last_name' => ($row[6] ?? ''),
+                'first_name' => ($row[7] ?? ''),
+                'barrio_id' => $barrio->id,
+                'parroquia_id' => $parroquia->id,
+                'cdc_id' => $cdc->id
+            ]);
+        }
+    }
+}
